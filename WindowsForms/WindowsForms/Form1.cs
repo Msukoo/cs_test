6c5a3d19fda22e7ff59adb4f6af93717dc700a45
minsu92 <rnalsttnn@gmail.com>
2020-12-29 18:52:34 +0900

    modify

---
+++ WindowsForms/WindowsForms/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        int cnt = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                ListData listData = ListData.Instance;
+
+                List<string> viewList = listData.list;
+
+                listBox.Items.Add(viewList[cnt]);
+
+                cnt++;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x.Message);
+                listBox.Items.Add("마지막 목록이 끝났습니다. ");
+
+                return;
+            }
+        }
+    }
+}
---
+++ WindowsForms/WindowsForms/ListData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsForms
+{
+    class ListData
+    {
+        public List<string> list;
+
+        private void SetList()
+        {
+            if (list == null)
+            {
+                list = new List<string>();
+                list.Add("blue");
+                list.Add("Yellow");
+                list.Add("red");
+            }
+        }
+
+        private ListData()
+        {
+            SetList();
+        }
+        //인스턴스 객체
+        private static ListData _instance;
+
+        //객체반환 함수
+        public static ListData Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new ListData();
+                return _instance;
+            }
+        }
+    }
+}
